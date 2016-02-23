d619f6fd0cad23e1521d7e4d075aa230d9f837b1
vadim-v-lebedev <vadim.v.lebedev@gmail.com>
2016-02-23 21:52:02 +0300

    trying to accomodate to a newest caffe version

--- config_processing.py
+++ config_processing.py
@@ -8,10 +8,10 @@ from paths import *
 
 def conv_layer(h, w, n, group=1, pad_h=0, pad_w=0, stride_h=1, stride_w=1):
     layer = caffe.proto.caffe_pb2.LayerParameter()
-    layer.type = caffe.proto.caffe_pb2.LayerParameter.LayerType.Value('CONVOLUTION')
+    layer.type = 'Convolution'
     #layer.convolution_param.engine = caffe.proto.caffe_pb2.ConvolutionParameter.CUDNN
     if (h == w):
-        layer.convolution_param.kernel_size = h
+        layer.convolution_param.kernel_size.append(h)
     else:
         layer.convolution_param.kernel_h = h
         layer.convolution_param.kernel_w = w
@@ -28,9 +28,9 @@ def conv_layer(h, w, n, group=1, pad_h=0, pad_w=0, stride_h=1, stride_w=1):
 
 def find_layer_by_name(model, layer_name):
     k = 0
-    while model.layers[k].name != layer_name:
+    while model.layer[k].name != layer_name:
         k += 1
-        if (k > len(model.layers)):
+        if (k > len(model.layer)):
             raise IOError('layer with name %s not found' % layer_name)
     return k
  
@@ -39,32 +39,38 @@ def accelerate_model(model, layer_to_decompose, rank):
     r = rank
     new_model = caffe.proto.caffe_pb2.NetParameter()
     for i in range(k):
-        new_model.layers.extend([model.layers[i]])
-    decomposed_layer = model.layers[k]
-    if decomposed_layer.type != decomposed_layer.LayerType.Value('CONVOLUTION'):
+        new_model.layer.extend([model.layer[i]])
+    decomposed_layer = model.layer[k]
+    if decomposed_layer.type != 'Convolution':
         raise AttributeError('only convolution layer can be decomposed')
-    param = decomposed_layer.convolution_param
-    new_model.layers.extend([conv_layer(1, 1, r)])
-    new_model.layers.extend([conv_layer(param.kernel_size, 1, r, r, pad_h=param.pad, stride_h=param.stride)])
-    new_model.layers.extend([conv_layer(1, param.kernel_size, r, r, pad_w=param.pad, stride_w=param.stride)])
-    new_model.layers.extend([conv_layer(1, 1, param.num_output)])
+    param = decomposed_layer.convolution_param   
+    if not hasattr(param, 'pad'):
+        param.pad = [0]
+    if param.pad == []:
+        param.pad.append(0)
+    if not hasattr(param, 'stride'):
+        param.stride = [1]
+    new_model.layer.extend([conv_layer(1, 1, r)])
+    new_model.layer.extend([conv_layer(param.kernel_size[0], 1, r, r, pad_h=param.pad[0], stride_h=param.stride[0])])
+    new_model.layer.extend([conv_layer(1, param.kernel_size[0], r, r, pad_w=param.pad[0], stride_w=param.stride[0])])
+    new_model.layer.extend([conv_layer(1, 1, param.num_output)])
     name = decomposed_layer.name
     for i in range(4):
-        new_model.layers[k+i].name = name + '-' + str(i + 1)
-        new_model.layers[k+i].bottom.extend([name + '-' + str(i)])
-        new_model.layers[k+i].top.extend([name + '-' + str(i + 1)])
-    new_model.layers[k].bottom[0] = model.layers[k].bottom[0]
-    new_model.layers[k+3].top[0] = model.layers[k].top[0]
-    for i in range(k+1, len(model.layers)):
-        new_model.layers.extend([model.layers[i]])
+        new_model.layer[k+i].name = name + '-' + str(i + 1)
+        new_model.layer[k+i].bottom.extend([name + '-' + str(i)])
+        new_model.layer[k+i].top.extend([name + '-' + str(i + 1)])
+    new_model.layer[k].bottom[0] = model.layer[k].bottom[0]
+    new_model.layer[k+3].top[0] = model.layer[k].top[0]
+    for i in range(k+1, len(model.layer)):
+        new_model.layer.extend([model.layer[i]])
     return new_model
 
 def create_deploy_model(model, input_dim=[64, 3, 32, 32]):
     new_model = caffe.proto.caffe_pb2.NetParameter()
     new_model.input.extend(['data'])
     new_model.input_dim.extend(input_dim)
-    for i in range(2,len(model.layers)-2):
-        new_model.layers.extend([model.layers[i]])
+    for i in range(2,len(model.layer)-2):
+        new_model.layer.extend([model.layer[i]])
     return new_model
     
 def load_model(filename):
@@ -99,7 +105,7 @@ def prepare_models(LAYER, R, NET_PATH, NET_NAME, INPUT_DIM):
     l = ind - 2#layer index in deploy version
     w = net.layers[l].blobs[0].data
     print w.shape
-    g = model.layers[ind].convolution_param.group
+    g = model.layer[ind].convolution_param.group
     if (g > 1):
         weights = np.zeros((w.shape[0], g * w.shape[1], w.shape[2], w.shape[3]))
         for i in range(g):
@@ -130,9 +136,9 @@ def prepare_models(LAYER, R, NET_PATH, NET_NAME, INPUT_DIM):
         f_c = np.random.standard_normal(f_c.shape)
         f_n = np.random.standard_normal(f_n.shape)
 
-    n = model.layers[ind].convolution_param.num_output
-    d = model.layers[ind].convolution_param.kernel_size
-    c = weights.shape[1]# / model.layers[ind].convolution_param.group #i don't know what i'm doing 
+    n = model.layer[ind].convolution_param.num_output
+    d = model.layer[ind].convolution_param.kernel_size
+    c = weights.shape[1]# / model.layer[ind].convolution_param.group #i don't know what i'm doing 
     
     f_y = np.reshape(f_y, [R, 1, d, 1])
     f_x = np.reshape(f_x, [R, 1, 1, d])
